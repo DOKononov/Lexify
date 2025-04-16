e9b5eb5aaa31c04b07a73d80193e9236c1d76dd1
d.o.kononov <d.o.kononov@gmail.com>
2025-04-16 09:35:59 +0300

    Initial Commit

---
+++ Lexify/LexifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexifyApp.swift
+//  Lexify
+//
+//  Created by Dmitry Kononov on 16.04.25.
+//
+
+import SwiftUI
+
+@main
+struct LexifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
